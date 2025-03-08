0c981d117c677b2fab4e74a5475b48888c359331
BlueMage <jihoopark7666@gmail.com>
2025-03-09 00:51:34 +0900

    🔧 ADD react image environment

---
+++ src/Example.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function Example() {
+  return (
+    <div>
+      <p>Example Loaded! maybe hot reloading is even available to new file!</p>
+    </div>
+  );
+}
+
+export default Example;
