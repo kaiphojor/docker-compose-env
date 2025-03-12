08eec5ce3dc62086a5acb8fcfd331e50379b34fb
BlueMage <jihoopark7666@gmail.com>
2025-03-13 00:34:29 +0900

    🗃️ ADD db - frontend data representation test

--- backend-express/server.js
+++ backend-express/server.js
@@ -15,9 +15,34 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB 연결 성공'))
   .catch(err => console.error('MongoDB 연결 실패:', err));
 
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true},
+  email: { type: String, required: true, unique: true},
+  createdAt: {type: Date, default: Date.now}
+});
+const User = mongoose.model('users',userSchema);
+
+const getUsers = async ()=>{
+  try{
+    const users = await User.find();
+    return users;
+  }catch(err){
+    console.error('조회 실패',err);
+    throw err;
+  }
+} 
+
 // 기본 라우트
-app.get('/', (req, res) => {
-  res.send('Express 백엔드 동작 중!');
+app.get('/', async (req, res) => {
+  try {
+    let users = await getUsers();
+    console.log(users);
+    // res.send('Express 백엔드 동작 중!');
+    res.json({message: 'Express 백엔드 동작 중!', users});
+  }catch(err){
+    console.error('조회 실패', err);
+    res.status(500).json({message: 'server error', error:err.message});
+  }
 });
 
 // 서버 시작
